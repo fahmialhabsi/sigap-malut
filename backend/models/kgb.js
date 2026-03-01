f7e898d1afb441b1c68f5cb8a983059e5feeb9d7
fahmialhabsi <cafedigitalsukse@gmai.com>
2026-03-02 00:32:07 +0900

    chore: convert remaining models to ESM using script

--- backend/models/kgb.js
+++ backend/models/kgb.js
@@ -1,19 +1,19 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/database.js';
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
 
 let Kgb;
 if (sequelize.models && sequelize.models.kgb) {
   Kgb = sequelize.models.kgb;
 } else {
   Kgb = sequelize.define(
-    'kgb',
+    "kgb",
     {
       id: { type: DataTypes.UUID, primaryKey: true },
       layanan_id: { type: DataTypes.STRING },
-      status: { type: DataTypes.STRING, defaultValue: 'draft' },
+      status: { type: DataTypes.STRING, defaultValue: "draft" },
       payload: { type: DataTypes.JSONB },
     },
-    { tableName: 'kgb' },
+    { tableName: "kgb" },
   );
 }
 
