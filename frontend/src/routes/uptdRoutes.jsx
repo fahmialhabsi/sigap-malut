0b90ed0719c4fe53c845ff5e0fbdd85f436600c6
fahmialhabsi <cafedigitalsukse@gmai.com>
2026-03-14 20:47:41 +0900

    feat(uptd): enable CRUD UPT-TKN via uptdRoutes (sidebar langsung ke ListPage, route lengkap CRUD)

--- frontend/src/routes/uptdRoutes.jsx
+++ frontend/src/routes/uptdRoutes.jsx
@@ -1,17 +1,54 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
 import UPTDBalaiPengawasanDashboard from "../pages/uptd-balai-pengawasan/Dashboard";
+import UPTTknListPage from "../modules/UPT-TKN/ListPage";
+import UPTTknFormPage from "../modules/UPT-TKN/FormPage";
+import UPTTknDetailPage from "../modules/UPT-TKN/DetailPage";
 
 export default function UptdRoutes() {
   return (
-    <Route
-      path="/module/uptd-balai-pengawasan"
-      element={
-        <ProtectedRoute>
-          <UPTDBalaiPengawasanDashboard />
-        </ProtectedRoute>
-      }
-    />
+    <Routes>
+      <Route
+        path="/module/uptd-balai-pengawasan"
+        element={
+          <ProtectedRoute>
+            <UPTDBalaiPengawasanDashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/uptd/UPT-TKN"
+        element={
+          <ProtectedRoute>
+            <UPTTknListPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/uptd/UPT-TKN/new"
+        element={
+          <ProtectedRoute>
+            <UPTTknFormPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/uptd/UPT-TKN/edit/:id"
+        element={
+          <ProtectedRoute>
+            <UPTTknFormPage isEdit />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/uptd/UPT-TKN/:id"
+        element={
+          <ProtectedRoute>
+            <UPTTknDetailPage />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
